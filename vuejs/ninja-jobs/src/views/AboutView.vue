493d38cfd4076bc396a44dae4f1e2d62aef24fd9
Bruno Simm <BRUNO.SIMM@edu.pucrs.br>
2022-07-04 18:10:16 -0300

    vuejs router & fetch

---
+++ vuejs/ninja-jobs/src/views/AboutView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint dolore,
+      harum animi beatae aperiam magnam fugiat recusandae nobis! Et quo
+      voluptate minima enim autem! Atque illo officiis aut error veritatis eaque
+      similique quo aliquid mollitia, ratione ipsa perspiciatis eius voluptatem
+      blanditiis culpa. Dignissimos repudiandae delectus sed? Reprehenderit amet
+      distinctio similique quos, numquam voluptas blanditiis laboriosam
+      molestiae dolor sed maxime doloremque, qui, quidem porro obcaecati odit
+      voluptate vel. Cupiditate debitis libero eius excepturi vero unde. Magni
+      officiis, hic illo magnam obcaecati harum ipsam impedit voluptas, quae
+      itaque excepturi repellat minima distinctio enim assumenda dignissimos
+      ratione vero culpa fugiat, quis eius autem!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique eum
+      hic inventore dolor molestiae error vitae facere. Quos dolores, neque
+      commodi consequatur et totam inventore veniam iusto assumenda qui eum
+      expedita? Corporis distinctio doloremque ea adipisci ex esse odit eos
+      illo. Error nisi reprehenderit similique quam omnis cupiditate nihil
+      nesciunt earum eaque itaque corporis repellat iste libero sunt numquam sit
+      quia, consequuntur dignissimos porro?
+    </p>
+  </div>
+</template>
---
+++ vuejs/ninja-jobs/src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <h1>HomePage</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati quasi
+      et recusandae debitis sequi. Earum vel asperiores esse optio officiis,
+      iure dolores quam rem, doloremque quae soluta voluptatem quas praesentium
+      nesciunt sapiente iste incidunt modi possimus voluptas repellat suscipit
+      consectetur quod? Dolores, animi harum quisquam aut sapiente quae sint
+      obcaecati maiores alias, minus vel provident quod adipisci officia facere
+      beatae ipsa expedita natus nobis minima recusandae libero hic magnam.
+      Impedit tempora rerum nemo rem sint iure neque dolorem, iste nisi unde
+      laboriosam veritatis explicabo animi recusandae qui quidem minus sed quis
+      esse harum veniam. Expedita eveniet repellendus porro non totam.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem repudiandae
+      voluptates sapiente, blanditiis officiis corrupti perferendis esse
+      molestiae, quidem possimus libero culpa nesciunt corporis debitis ipsam ad
+      repellat obcaecati, ipsum alias illum at numquam aspernatur inventore!
+      Laboriosam, nesciunt! Praesentium enim iure eos suscipit sed ex id quae et
+      veritatis cum, repellendus a iste asperiores unde ipsam quos, laudantium
+      nobis facere aspernatur dicta ducimus hic ipsum libero. Eum distinctio
+      doloremque cupiditate vel, facilis enim minima, repellendus quibusdam
+      perferendis eveniet voluptates inventore!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+  components: {},
+};
+</script>
